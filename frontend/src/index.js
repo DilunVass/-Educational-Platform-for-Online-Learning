c366783bda12cc14f290c6d9dd6a9d576d1ec593
Chamalka Marasinghe <Chamalka.Marasinghe@app360.lk>
2024-05-05 00:15:19 +0530

    client front end branch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,3 +16,5 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//client front end branch
