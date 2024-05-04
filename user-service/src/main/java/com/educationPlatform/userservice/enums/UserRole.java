51d35e3a88a4efa6f4d1ded904bf4ed81ae2e9f6
DilunVass <100837990+DilunVass@users.noreply.github.com>
2024-05-05 01:16:23 +0530

    user service

---
+++ user-service/src/main/java/com/educationPlatform/userservice/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.educationPlatform.userservice.enums;
+
+public enum UserRole {
+    STUDENT("Student"), INSTRUCTOR("Instructor"), ADMIN("Admin");
+
+    private final String userRole;
+    UserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public  String getUserRole(){
+        return userRole;
+    }
+}
+
+
