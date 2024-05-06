8d3f20c48c62b7e8cfeb96041f7d63e982b6c40c
IT21268076 <it21268076@my.sliit.lk>
2024-05-06 13:36:43 +0530

    course_service updated

--- course_service/src/main/java/com/example/course_service/Dtos/CourseDto.java
+++ course_service/src/main/java/com/example/course_service/Dtos/CourseDto.java
@@ -1,11 +1,15 @@
 package com.example.course_service.Dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseDto {
     @Id
     private String courseId;
--- course_service/src/main/java/com/example/course_service/Utils/ApiResponse.java
+++ course_service/src/main/java/com/example/course_service/Utils/ApiResponse.java
@@ -1,8 +1,12 @@
 package com.example.course_service.Utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
     // Getters and setters
     private T data;
