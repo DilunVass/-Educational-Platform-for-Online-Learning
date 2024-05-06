57fd4841be49e66614a9c17af34a9e21d09fac31
Chamoda Karunathilaka <99285844+IT21268076@users.noreply.github.com>
2024-05-06 13:48:41 +0530

    Update Course.java

--- microservices/course_service/src/main/java/com/example/course_service/Models/Course.java
+++ microservices/course_service/src/main/java/com/example/course_service/Models/Course.java
@@ -1,12 +1,16 @@
 package com.example.course_service.Models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "courses")
 public class Course {
     @Id
