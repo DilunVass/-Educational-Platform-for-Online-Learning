6be24b9bc981d67ee8366164a9223ef3836d92c3
Chamoda Karunathilaka <it21268076@my.sliit.lk>
2024-05-08 00:51:53 +0530

    progress updated

--- microservices/personalize_learning_service/src/main/java/com/example/personalize_learning_service/Repositories/ProgressRepository.java
+++ microservices/personalize_learning_service/src/main/java/com/example/personalize_learning_service/Repositories/ProgressRepository.java
@@ -1,4 +1,7 @@
 package com.example.personalize_learning_service.Repositories;
 
-public interface ProgressRepository {
+import com.example.personalize_learning_service.Models.Progress;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProgressRepository extends MongoRepository<Progress, Long> {
 }
