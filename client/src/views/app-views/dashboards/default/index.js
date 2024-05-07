5ee7304c0914681b40ab111e8b5f47ad558ab446
Chamalka Marasinghe <Chamalka.Marasinghe@app360.lk>
2024-05-07 23:48:47 +0530

    client side initial commitment

---
+++ client/src/views/app-views/dashboards/default/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+    Avatar,
+    Badge,
+    Card,
+    Table,
+    Tag,
+    Tooltip,
+    message,
+    Button,
+    List,
+    Popover,
+    Row,
+    Col,
+    Select,
+    Form,
+    Divider,
+    notification,
+} from "antd";
+import {
+    EyeOutlined,
+    DeleteOutlined,
+    EditOutlined,
+    UsergroupDeleteOutlined,
+    CheckOutlined,
+} from "@ant-design/icons";
+
+const DefaultDashboard = () => {
+    return (
+        <>
+            home
+        </>
+    );
+};
+
+export default DefaultDashboard;
