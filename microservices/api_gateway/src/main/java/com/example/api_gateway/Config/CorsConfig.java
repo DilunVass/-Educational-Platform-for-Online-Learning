d637af485066adc58745a24be63ae4a495641587
Chamoda Karunathilaka <it21268076@my.sliit.lk>
2024-05-09 22:38:52 +0530

    api gateway updated

---
+++ microservices/api_gateway/src/main/java/com/example/api_gateway/Config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.api_gateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
